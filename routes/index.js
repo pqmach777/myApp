1d30d7d796b61c44ae709d3280721b9bb5c866d7
Phong Mach <phongmach@phongs-mbp.attlocal.net>
2019-01-11 20:28:20 -0600

    added socket.io for potential chatbox. more styling and partials

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,32 @@ var data = require('../data/data.json')
 
 router.get('/', (req, res)=>{
     var musicians = data.musicians;
-    var picture = []
+    var picture = [];
+    var artistName = [];
 
+    var news = []
+    var newsContent = []
+    var newsTitle = []
+    var tour = []
     musicians.forEach((item)=>{
         picture = picture.concat(item.shortname);
-    })
+        artistName = artistName.concat(item.name);
+        news = news.concat(item.news);
+        tour = tour.concat(item.tour)
+    });
 
+    for(i=0; i < news.length; i++){
+        newsTitle.push(news[i].title);
+        newsContent.push(news[i].content);
+    }
+  
+
+    
     res.render('index',{
+        newsTitle: newsTitle,
+        newsContent: newsContent,
         pageTitle: 'Muse',
+        name: artistName,
         shortname: picture,
         musicians: musicians,
         pageID: 'home'
